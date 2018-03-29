daa70bcd00738fef39eeedad40aa20fa39b84dda
alexander.ovchinnikov <alexander.ovchinnikov@stark.games>
2018-03-29 17:36:35 +0300

    Separator. Relative path fix

--- FilemapGenerator.py
+++ FilemapGenerator.py
@@ -11,7 +11,7 @@ class FileStats:
     version = None
 
     def getModDate(self):
-        return os.path.getmtime(self.abspath)
+        return str(int(os.path.getmtime(self.abspath)))
 
     def getMd5(self):
         hash_md5 = md5()
@@ -20,13 +20,15 @@ class FileStats:
         return hash_md5.hexdigest()
 
     def __init__(self, path, base):
-        self.relpath = path
+        #print(path)
+        #print(base)
+        self.relpath = os.path.relpath(path,base)
         self.abspath = os.path.join(base, path)
         self.md5  = self.getMd5()
         self.dateMod = self.getModDate()
 
     def __str__(self):
-        return ",".join([self.relpath, self.md5, self.dateMod])
+        return ";".join([self.relpath, self.md5, self.dateMod])
 
 
 def GetStats(files,base):
@@ -39,10 +41,12 @@ def Main(path):
     fileStats = GetStats(GetFiles(path),path)
     for f in fileStats:
         print(f)
-        print("\n")
 
 if (len(sys.argv)<=1):
     raise Exception("No path provided")
 else:
     path = sys.argv[1]
+    if (path[0]!="/" and path[0]!="~"):
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)),path)
+    print(path)
     Main(path)
